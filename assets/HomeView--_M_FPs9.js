7e7b601475180162daf18a19803c66b90a76351b
owendong527 <owendong900995@gmail.com>
2024-03-05 00:09:51 +0800

    adding dist

---
+++ assets/HomeView--_M_FPs9.js
@@ -0,0 +1,1 @@
+import{_ as e,o,c as s,a as c}from"./index-3Ds7k2Tn.js";const t={},_={class:"home"},a=c("h1",null,"Hello, This is Home Page.",-1),n=[a];function r(i,l){return o(),s("div",_,n)}const h=e(t,[["render",r]]);export{h as default};
---
+++ assets/OrderView-L40jZC9U.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as r}from"./index-3Ds7k2Tn.js";const n={};function o(t,s){return c(),r("h2",null,"訂單列表")}const a=e(n,[["render",o]]);export{a as default};
